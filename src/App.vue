ee9cfae5ad480449387906dfc12d9d5314ab1bbf
aritonkaramani <ariton.karamani@gmail.com>
2022-05-04 23:51:30 +0200

    added small structure for how employees should be displayed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div></div>
+  <div class="app-wrapper">
+    <input v-model="search_query" />
+    <div class="employee-wrapper">
+      <info-card
+        v-for="(employee, index) in employees"
+        :key="index"
+        :employee="employee"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import InfoCard from "./components/InfoCard.vue";
 export default {
   name: "App",
+  components: {
+    InfoCard,
+  },
+  data() {
+    return {
+      employees: [],
+      search_query: "",
+    };
+  },
   mounted() {
     this.getEmployees();
-    console.log(process.env);
   },
   methods: {
     async getEmployees() {
@@ -26,7 +44,6 @@ export default {
           employee.name = "";
         }
       });
-      console.log(this.employees);
     },
   },
 };
---
+++ src/components/InfoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ employee.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmployeeCard",
+  props: {
+    employee: Object,
+  },
+  mounted() {
+    console.log(this.employee);
+  },
+};
+</script>
+
+<style></style>
